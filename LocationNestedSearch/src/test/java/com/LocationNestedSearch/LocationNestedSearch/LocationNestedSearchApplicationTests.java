4637515aceb21a17dca41cc469ab2a86b2dd6c66
BernardoAlves05 <116270830+BernardoAlves05@users.noreply.github.com>
2022-11-02 10:11:29 +0000

    Add files via upload

---
+++ LocationNestedSearch/src/test/java/com/LocationNestedSearch/LocationNestedSearch/LocationNestedSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LocationNestedSearch.LocationNestedSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationNestedSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
